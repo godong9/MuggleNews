ce2dd060d6079588c84e07271ba91bd529fda50b
Donghyun Go <godong9@gmail.com>
2016-12-04 00:12:00 +0900

    Update login button

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  var data = {};
+  data.me = req.user && req.user._json;
   // console.log(req.user._json);
-  res.render('index', { title: 'Express' });
-
+  res.render('index', data);
 });
 
 module.exports = router;
