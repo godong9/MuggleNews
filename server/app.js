62cf7abc08bb52e4bb7b923c71372c574e43843a
Donghyun Go <godong9@gmail.com>
2016-12-04 03:05:24 +0900

    Add common_head

--- server/app.js
+++ server/app.js
@@ -30,8 +30,7 @@ var sessionStore = new MySQLStore({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }));
