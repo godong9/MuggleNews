c86f7d92674a246999b1934bd7c835caa7b9c334
Donghyun Go <godong9@gmail.com>
2016-12-04 03:28:13 +0900

    Update timeline page

--- server/controllers/timelines.js
+++ server/controllers/timelines.js
@@ -10,14 +10,17 @@ const TimelineController = {
   getTimelinePage: function login(req, res) {
     Timeline.getTimelineById(req.params.id, function(err, timeline) {
       let data = {};
-      if (err) {
-        return res.redirect('/page/error');
-      }
-      if (!timeline) {
-        return res.status(404).send('Not Found!');
-      }
+      // if (err) {
+      //   return res.redirect('/page/error');
+      // }
+      // if (!timeline) {
+      //   return res.status(404).send('Not Found!');
+      // }
       View.setCommonData(req, data);
-      res.send('timeline', data);
+      data.timeline = timeline;
+      // data.commentPage = 'timelines#' + timeline.id;
+      data.commentPage = 'timelines#1';
+      res.render('timeline', data);
     });
   }
 };
