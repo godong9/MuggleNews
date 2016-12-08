4d29188d849eabd4c5b7fe774d75c0716480b0ac
Donghyun Go <godong9@gmail.com>
2016-12-08 22:11:04 +0900

    Update preview api

---
+++ server/controllers/previews.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const _ = require('underscore');
+const logger = require('log4js').getLogger('controllers/timelines');
+const Preview = require('../models/previews');
+
+const PreviewController = {
+  postPreview: function login(req, res) {
+
+  }
+};
+
+module.exports = PreviewController;
---
+++ server/models/previews.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const pool = require('../db/db').pool;
+
+let Preview = {
+
+};
+
+module.exports = Preview;
