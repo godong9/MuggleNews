e69f0f16486d6a4a06d899d06af844a4b2355e15
Donghyun Go <godong9@gmail.com>
2016-12-17 10:47:22 +0900

    Update crawler

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,20 @@ const express = require('express');
 const router = express.Router();
 const View = require('../services/view');
 
+// const Crawler = require('../services/crawler');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   let data = {};
   View.setCommonData(req, data);
   // console.log(req.user._json);
   res.render('index', data);
 });
+
+// router.get('/test', function(req, res) {
+//   Crawler.getPreviewData('http://www.naver.com', function(err, body) {
+//     res.send(body);
+//   });
+// });
 
 module.exports = router;
