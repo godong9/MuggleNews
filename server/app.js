1815ce08d4ee3239b4620fb50f827238ad4eb96b
GoDonghyun <godong9@gmail.com>
2017-03-12 12:31:54 +0900

    Update

--- server/app.js
+++ server/app.js
@@ -39,9 +39,9 @@ app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser({limit: '10mb'}));
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+app.use(bodyParser({limit: '30mb'}));
+app.use(bodyParser.json({ limit: '30mb' }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
